278e0a89284c1f9f525af1a9501effdeff880466
Stepa <stepan.kozov@gmail.com>
2020-03-18 16:33:07 +0300

    commit1

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask_ngrok import run_with_ngrok
+import os
 from flask import Flask
 
 app = Flask(__name__)
-run_with_ngrok(app)
+
 
 @app.route('/')
 def index():
@@ -10,5 +10,5 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run()
-
+    port = int(os.environ.get("POST", 5000))
+    app.run(host='0.0.0.0', port=port)
